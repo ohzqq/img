617cad35a260549333ceb5ad6f8326635a657553
ohzqq <iamchurkey@gmail.com>
2025-12-23 11:25:29 -0500

    decode

---
+++ decode.go
@@ -0,0 +1,50 @@
+package img
+
+import (
+	"image"
+	"io"
+	"os"
+)
+
+// Decode reads an image from r.
+// If want to use custom image format packages which were registered in image package, please
+// make sure these custom packages imported before importing imgconv package.
+// https://github.com/disintegration/imaging
+func Decode(r io.Reader) (image.Image, error) {
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+// DecodeConfig decodes the color model and dimensions of an image that has been encoded in a
+// registered format. The string returned is the format name used during format registration.
+// https://github.com/sunshineplan/imgconv
+func DecodeConfig(r io.Reader) (image.Config, string, error) {
+	return image.DecodeConfig(r)
+}
+
+// Open loads an image from file.
+// https://github.com/sunshineplan/imgconv
+func Open(file string) (image.Image, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return Decode(f)
+}
+
+// OpenAll loads images from files.
+func OpenAll(files []string) ([]image.Image, error) {
+	imgs := make([]image.Image, len(files))
+	for i, file := range files {
+		img, err := Open(file)
+		if err != nil {
+			return imgs, err
+		}
+		imgs[i] = img
+	}
+	return imgs, nil
+}
